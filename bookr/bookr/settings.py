45612beba54d7abb0c551e6a45bddd1a8d52b8c9
Bailee Gerlich <bngerlich2@buffs.wtamu.edu>
2023-09-08 14:30:27 -0500

    Exercise 2.04

--- bookr/bookr/settings.py
+++ bookr/bookr/settings.py
@@ -79,6 +79,15 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
+#     new database
+#     "default":{
+#         'ENGINE: django.db.backends.postgresql',
+#         'NAME' : 'bookr',
+#         'USER' : <username>,
+#         'PASSWORD' : <password>,
+#         'HOST' : <host-IP-address>,
+#         'PORT' : '5432',
+#     }
 }
 
 
