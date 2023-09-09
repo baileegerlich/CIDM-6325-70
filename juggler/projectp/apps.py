43e89b0f11528a45f60f6bcc455c6248c2933437
Bailee Gerlich <bngerlich2@buffs.wtamu.edu>
2023-09-09 14:30:36 -0500

    Make Migrations for Actibity 2.01

--- juggler/projectp/apps.py
+++ juggler/projectp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ProjectmConfig(AppConfig):
+class ProjectConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "projectm"
+    name = "projectp"
